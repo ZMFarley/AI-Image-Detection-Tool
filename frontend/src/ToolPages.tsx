46b72a4af1c0c7f384200ee2512171ddc80a954f
ZMFarley <zacharymfarley@gmail.com>
2026-07-05 16:12:27 -0400

    adding css changes and functionality to prevent users from uploading no image and continuing analysis

--- frontend/src/ToolPages.tsx
+++ frontend/src/ToolPages.tsx
@@ -79,23 +79,23 @@ export default function UploadFile() {
                     </main>
                     <div className="button-container">
                         <button className="delete-button" onClick={handleDeleteImage}>Delete Image</button>
-                        <button className="transition-button" onClick={uploadImage}>Click To Submit to the detector</button>
+                        <button className="transition-button" onClick={uploadImage} disabled={!file}>Click To Submit to the detector</button>
                     </div>
                 </div> )}
 
                 {/*Loading screen to prevent additional inputs while the client is waiting for response*/}
                 {page === "loading" && (
-                <div className="center">
+                <div className="container">
                     {/*SVG sourced from https://magecdn.com/tools/svg-loaders/cog05/*/}
                     {image && (<img alt="loading spinner" src={loadingSpinner} width="500" height="500"/>)}
                 </div> )}
                 
                 {/*Section for model prediction output*/}
                 {page === "analysis"  && (
-                <div className="center">
+                <div className="page">
                     <h1 className="title">Results</h1>
                     {/*Output section for results*/}
-                    <div className="center">
+                    <div className="container">
                         {/*Conditionals to output model predictions*/}
                         {response?.result === 0  && <h2>The model has predicted this image is NOT AI Generated</h2>}
                         {response?.result === 1  && <h2>The model has predicted this image is AI Generated</h2>}
